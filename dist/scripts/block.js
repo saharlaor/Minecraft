06e4be078a37551316b3cc52c9b7b114493a0aa5
saharoori <saharlaor@gmail.com>
2021-12-02 22:35:23 +0200

    Block module with types and class

---
+++ dist/scripts/block.js
@@ -0,0 +1,36 @@
+const BLOCK_TYPES = {
+    dirt: "dirt",
+    empty: "empty",
+    grass: "grass",
+    leaves: "leaves",
+    stone: "stone",
+    wood: "wood",
+};
+
+export const Block = class {
+    constructor(blockType, x, y, htmlEl) {
+        this.blockType = blockType;
+        this.x = x;
+        this.y = y;
+        this.htmlEl = htmlEl;
+    }
+
+    // Getters
+    get type() {
+        return this.blockType;
+    }
+    get coordinates() {
+        return [y, x];
+    }
+
+    // Setters
+    set type(newType) {
+        this.blockType = newType;
+        this.htmlEl.classList.forEach((className) => {
+            if (className != "block") {
+                this.htmlEl.classList.remove(className);
+            }
+        });
+        this.htmlEl.classList.remove(newType);
+    }
+};
